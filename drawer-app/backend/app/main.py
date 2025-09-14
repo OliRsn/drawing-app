087cb9ba46ef6014c0d7ff7ca21cdd39ad47cfcc
OliRsn <32337273+OliRsn@users.noreply.github.com>
2025-09-14 22:20:11 +0200

    feat: initialize FastAPI backend

---
+++ drawer-app/backend/app/main.py
@@ -0,0 +1,57 @@
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/classrooms/", response_model=schemas.Classroom)
+def create_classroom(classroom: schemas.ClassroomCreate, db: Session = Depends(get_db)):
+    return crud.create_classroom(db=db, classroom=classroom)
+
+
+@app.get("/classrooms/", response_model=list[schemas.Classroom])
+def read_classrooms(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    classrooms = crud.get_classrooms(db, skip=skip, limit=limit)
+    return classrooms
+
+
+@app.get("/classrooms/{classroom_id}", response_model=schemas.Classroom)
+def read_classroom(classroom_id: int, db: Session = Depends(get_db)):
+    db_classroom = crud.get_classroom(db, classroom_id=classroom_id)
+    if db_classroom is None:
+        raise HTTPException(status_code=404, detail="Classroom not found")
+    return db_classroom
+
+
+@app.post("/classrooms/{classroom_id}/students/", response_model=schemas.Student)
+def create_student_for_classroom(
+    classroom_id: int, student: schemas.StudentCreate, db: Session = Depends(get_db)
+):
+    return crud.create_student(db=db, student=student, classroom_id=classroom_id)
+
+
+@app.get("/students/", response_model=list[schemas.Student])
+def read_students(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    students = crud.get_students(db, skip=skip, limit=limit)
+    return students
+
+
+@app.post("/students/{student_id}/grades/", response_model=schemas.Grade)
+def create_grade_for_student(
+    student_id: int, grade: schemas.GradeCreate, db: Session = Depends(get_db)
+):
+    return crud.create_student_grade(db=db, grade=grade, student_id=student_id)
