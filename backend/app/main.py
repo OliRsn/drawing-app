6627fdc9a5070b5ff86a7340a31a63ea64f8b038
OliRsn <32337273+OliRsn@users.noreply.github.com>
2025-09-28 19:40:36 +0200

    enhance backend config for https

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,9 +12,16 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(root_path="/api")
 
+origins = [
+    "http://localhost",            
+    "http://127.0.0.1",           
+    "https://luckystudent.cloud",  
+    "https://www.luckystudent.cloud",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
